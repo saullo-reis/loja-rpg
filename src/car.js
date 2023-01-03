e71e7030173aa396d36253ca782d17cb979bf294
Saullo Reis <saulloreis01@hotmail.com>
2023-01-03 13:06:30 -0300

    03-01-23 / 2 commit

--- src/car.js
+++ src/car.js
@@ -1,40 +1,44 @@
 import { Section } from "./car-styles";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
+export const Car = ({ parentToChild }) => {
+  let arrayCar = parentToChild;
+  var soma = 0;
 
-export const Car = ({parentToChild}) =>{
-    let arrayCar = parentToChild
-    var soma = 0;
+  for (var i = 0; i < arrayCar.length; i++) {
+    soma += arrayCar[i].price;
+  }
 
-    for(var i = 0; i < arrayCar.length; i++) {
-        soma += arrayCar[i].price;
-    }
-    
-    return(
-        <Section>
-            <ul>
-                {
-                    arrayCar.length == 0 ? <h1>Nenhum item adicionado ao carrinho</h1> :
-                    arrayCar.map((element, index) => {
-                    return(
-                        <li key={index}>
-                            <h2>{element.nome}</h2>
-                            <img src={require(`${element.image}`)} alt="imagem" />
-                            <h2>Preço: {element.price}</h2>
-                            <p>{element.desc}</p>
-                        </li>
-                    )
-                })
-
-                }
-            </ul>
-            <h2>Preço total da compra: {soma} Golds</h2>
-            {
-              arrayCar.length >= 1 ? <Link to={"/"}><button onClick={() => {window.alert('Compra Finalizada')}}>Finalizar compra</button></Link> : null
-            }
-            
-        </Section>
-    )
-
-}   
-
+  return (
+    <Section>
+      <ul>
+        {arrayCar.length == 0 ? (
+          <h1>Nenhum item adicionado ao carrinho</h1>
+        ) : (
+          arrayCar.map((element, index) => {
+            return (
+              <li key={index}>
+                <h2>{element.nome}</h2>
+                <img src={require(`${element.image}`)} alt="imagem" />
+                <h2>Preço: {element.price}</h2>
+                <p>{element.desc}</p>
+              </li>
+            );
+          })
+        )}
+      </ul>
+      <h2>Preço total da compra: {soma} Golds</h2>
+      {arrayCar.length >= 1 ? (
+        <Link to={"/"}>
+          <button
+            onClick={() => {
+              window.alert("Compra Finalizada");
+            }}
+          >
+            Finalizar compra
+          </button>
+        </Link>
+      ) : null}
+    </Section>
+  );
+};
